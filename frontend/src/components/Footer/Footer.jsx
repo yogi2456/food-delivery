0b382bead7857e6c5e187a2f5e695c06541e10ca
yogi2456 <138296327+yogi2456@users.noreply.github.com>
2024-05-02 22:06:00 +0530

    updates

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -8,28 +8,28 @@ const Footer = () => {
       <div className="footer-content">
         <div className="footer-content-left">
             <img src={assets.logo} alt=''/>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-            {/* <div className="footer-social-icons">
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+            <div className="footer-social-icons">
                 <img src={assets.facebook_icon} alt="" />
                 <img src={assets.twitter_icon} alt="" />
                 <img src={assets.linkedin_icon} alt="" />
-            </div> */}
+            </div>
         </div>
         <div className="footer-content-center">
-            {/* <h2>Company</h2>
+            <h2>Company</h2>
             <ul>
                 <li>Home</li>
                 <li>About us</li>
                 <li>Delivery</li>
                 <li>Privacy Policy</li>
-            </ul> */}
+            </ul>
         </div>
         <div className="footer-content-right">
-            {/* <h2>GET IN TOUCH</h2>
+            <h2>GET IN TOUCH</h2>
             <ul>
                 <li>+1-212-456-7890</li>
                 <li>contact@tomato.com</li>
-            </ul> */}
+            </ul>
         </div>
       </div>
       <hr/>
